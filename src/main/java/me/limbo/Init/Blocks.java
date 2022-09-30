b89d75683e90c858a8867e1d90ce8dccc246f1cd
duykhanh.ng1 <duykhanh.ng1@samsung.com>
2022-09-30 18:00:54 +0700

    coding

--- src/main/java/me/limbo/Init/Blocks.java
+++ src/main/java/me/limbo/Init/Blocks.java
@@ -1,7 +1,12 @@
 package me.limbo.Init;
 
+import java.util.List;
+
 import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.configuration.ConfigurationSection;
+
+import me.limbo.Prison;
 
 public class Blocks {
 	Location location;
@@ -10,5 +15,13 @@ public class Blocks {
 	Blocks(Location loc, Block block){
 		this.location = loc;
 		this.block = block;
+	}
+	
+	List<Blocks> load(){
+		ConfigurationSection con = Prison.getIntance().getConfig().getConfigurationSection("oldBlocks");
+		if(con == null) return null;
+		
+		for (String string : con.getKeys(false)) {
+		}
 	}
 }
